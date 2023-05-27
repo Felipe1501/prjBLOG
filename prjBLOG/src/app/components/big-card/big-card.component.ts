fefabf1ca2c458e4fe84e99ff19b3e6cab275d5a
Felipe1501 <felipebsantos1501@gmail.com>
2023-05-27 18:47:17 -0300

    criando components e personalizando title do blog

---
+++ prjBLOG/src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.html']
+})
+export class BigCardComponent implements OnInit {
+    
+    constructor(){}
+    
+    
+    ngOnInit(): void {
+        
+    }
+  
+}
