ecc3f571a8e8d9fbeb7bf1f6380cec2624cf2bca
Felipe1501 <felipebsantos1501@gmail.com>
2023-05-27 22:17:45 -0300

    criando component de big-card

--- prjBLOG/src/app/components/big-card/big-card.component.ts
+++ prjBLOG/src/app/components/big-card/big-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.html']
+  styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
     
